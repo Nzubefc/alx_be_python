38e603418c6280afb394711929e32cbafab95edc
Nzube Obiesie <nzubeobiesie28@gmail.com>
2025-09-11 18:43:22 +0100

    modified folder

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1 = int(input("Enter the first number: ")) 
-number2 = int(input("Enter the second number: "))
+number1 = 10
+number2 = 5
 sum_result = number1 + number2
 print("The sum  is:", sum_result)
-difference_result = number1 - number2
+difference_result = number1 - number2 
 print("The difference is:", difference_result)
 product_result = number1 * number2
 print("The product is:", product_result)
